6a65e51b214b33d4d29b0adb9d93a4e24040bbe3
gccccc <wakata@11bnb.com>
2015-12-11 00:53:10 +0800

    Create sample1.py

---
+++ sample1.py
@@ -0,0 +1,15 @@
+#!/usr/bin
+#encoding:utf-8
+
+class sample1:
+	def sum(self,a,b):
+		return a+b
+	def sub(self,a,b):
+		return a-b
+	def mul(self,a,b):
+		return a*b
+	def div(self,a,b):
+            try:
+		return a/b
+            except:
+                pass
